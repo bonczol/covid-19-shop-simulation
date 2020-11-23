140cc96260bd65bc703ed8d95f7c71a7605bf396
wkupaj <wkupaj@gmail.com>
2020-11-23 23:01:09 +0100

    Added correct list of shelfs

--- model.py
+++ model.py
@@ -14,14 +14,14 @@ class CovidModel(Model):
         self.schedule = RandomActivation(self)
         self.height = height
         self.width = width
-        self.shelfs=[]
+        self.shelfs = []
         self.shop_map = ShopMap(rows=2,
-                           shelves_in_row=5,
-                           shelf_length=10,
-                           space_between_rows=3,
-                           space_between_shelves=2)
+                                shelves_in_row=5,
+                                shelf_length=10,
+                                space_between_rows=3,
+                                space_between_shelves=2)
 
-# TODO dodac zbieranie statystyk
+        # TODO dodac zbieranie statystyk
         self.sick = 0
         self.datacollector = DataCollector(
             {"sick": "sick"},  # Model-level count of happy agents
@@ -29,21 +29,25 @@ class CovidModel(Model):
             {"x": lambda a: a.pos[0], "y": lambda a: a.pos[1]},
         )
 
-
-# TODO ulepszyc ustawinie poczatkowej mapki
-        h,w = self.shop_map.shop_map.shape
+        # TODO ulepszyc ustawinie poczatkowej mapki
+        h, w = self.shop_map.shop_map.shape
         for i in range(h):
             for j in range(w):
-                type =self.shop_map.shop_map[i,j]
+                type = self.shop_map.shop_map[i, j]
                 a = h - 1 - i
                 if type != 0:
-                    agent = BackgroundAgent(i, self, (j,a), type)
+                    agent = BackgroundAgent(i, self, (j, a), type)
                     self.grid.position_agent(agent, j, a)
 
-                if type==1:
-                    self.shelfs.append((j,a))
+                if type == 1:
+                    if j-1 > -1 and self.shop_map.shop_map[i, j-1] == 0:
+                        self.shelfs.append((j-1, a))
+                    elif j+1<w and self.shop_map.shop_map[i, j+1] == 0:
+                        self.shelfs.append((j+1, a))
+                    elif self.shop_map.shop_map[i +1, j] == 0:
+                        self.shelfs.append((j, a - 1))
 
-# TODO poprawic wstawianie agentow na poczatek
+        # TODO poprawic wstawianie agentow na poczatek
         for i in range(N):
             pos = self.grid.find_empty()
             agent = CustomerAgent(i, self, pos, True, True)
@@ -54,8 +58,7 @@ class CovidModel(Model):
         self.datacollector.collect(self)
 
     def step(self):
-# TODO
+        # TODO
         self.schedule.step()
         # collect data
         self.datacollector.collect(self)
-
